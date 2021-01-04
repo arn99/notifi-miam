8eb237bf1ea05193535d37f1ce6f20a8ab167f0c
rams99 <aroldrams@gmail.com>
2021-01-04 16:54:08 +0000

    last upadte

--- route/route.js
+++ route/route.js
@@ -10,6 +10,7 @@ const AWS = require('aws-sdk');
 var queueUrl = "https://sqs.us-east-1.amazonaws.com/073844720199/notify-order";
 var isDev = true;
 const frontend = 'https://miam-bf.netlify.app'
+const app = express();
 app.use(express.json());
 app.use(cors({origin: frontend}));
 if (process.env.NODE_ENV == "production") {
